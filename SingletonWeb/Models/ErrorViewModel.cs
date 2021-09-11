06d31f1a64618f530ee71309815ab965124e0c5b
Souldev23 <soul23k@hotmail.com>
2021-09-11 15:54:38 -0500

    Agregar archivos de proyecto.

---
+++ SingletonWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SingletonWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
